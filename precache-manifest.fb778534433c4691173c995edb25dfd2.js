2f33d9fe36278e819d3d64c97e9a910fde573724
Orel <orelokavi@gmail.com>
2020-06-11 01:08:29 +0300

    Updates

---
+++ precache-manifest.fb778534433c4691173c995edb25dfd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "503e6cd458397c194be1616b60cb35ef",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "217f82106dcfac255bc2",
+    "url": "/monsters-rolodex/static/css/main.fd6e0faa.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217f82106dcfac255bc2",
+    "url": "/monsters-rolodex/static/js/main.a8c340cd.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
